dcbd434c83aeb916e4a62d6c364c2a3d42f75afe
egalpin <egalpin@Evans-MacBook-Pro.local>
2015-11-25 16:49:40 -0500

    Added explicit call to python in install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #! /usr/bin/env sh
 
 start_dir = `pwd`
+home = $( echo $HOME )
 
-curl -fLo ~/apt-vim/apt-vim --create-dirs \
+curl -fLo ${home}/apt-vim/apt-vim --create-dirs \
     https://raw.githubusercontent.com/egalpin/apt-vim/29_ENH_one_touch_setup/apt-vim
 
-curl -fLo ~/apt-vim/vim_config.json --create-dirs \
+curl -fLo ${home}/apt-vim/vim_config.json --create-dirs \
     https://raw.githubusercontent.com/egalpin/apt-vim/29_ENH_one_touch_setup/vim_config.json
 
-cd ~/apt-vim
-sudo ./apt-vim init
+cd ${home}/apt-vim
+sudo python ${home}/apt-vim/apt-vim init
 cd $start_dir
