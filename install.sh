9aaf30b073db910811759927a5f12149241759ce
egalpin <egalpin@Evans-MacBook-Pro.local>
2015-11-26 10:02:49 -0500

    Added most install functionality to install.sh rather than apt-vim functions

--- install.sh
+++ install.sh
@@ -1,14 +1,44 @@
 #! /usr/bin/env sh
 
-start_dir=`pwd`
-home=$( echo $HOME )
+start_dir=$(pwd)
+log_file="${HOME}/apt-vim/install.log"
 
-curl -fLo ${home}/apt-vim/apt-vim --create-dirs \
+# Redirect all output to install log
+exec 5<&1
+exec 6<&2
+exec 1> $log_file 2>&1
+
+echo
+
+if grep -q "export PATH=${PATH}:${HOME}/.vimpkg/bin" then
+    echo "export PATH=${PATH}:${HOME}/.vimpkg/bin" >> /etc/profile
+fi
+
+# Download the apt-vim files
+curl -fLo ${HOME}/apt-vim/apt-vim --create-dirs \
     https://raw.githubusercontent.com/egalpin/apt-vim/29_ENH_one_touch_setup/apt-vim
 
-curl -fLo ${home}/apt-vim/vim_config.json --create-dirs \
+curl -fLo ${HOME}/apt-vim/vim_config.json \
     https://raw.githubusercontent.com/egalpin/apt-vim/29_ENH_one_touch_setup/vim_config.json
 
-cd ${home}/apt-vim
-sudo python ${home}/apt-vim/apt-vim init
+# Add vimrc if there isn't one already
+[ -f ${HOME}/.vimrc ] || touch ${HOME}/.vimrc
+
+# Make sure vimrc is using pathogen
+if grep -q "execute pathogen#infect()" then
+    echo "execute pathogen#infect()" >> ${HOME}/.vimrc
+fi
+if grep -q "call pathogen#helptags()" then
+    echo "call pathogen#helptags()" >> ${HOME}/.vimrc
+fi
+
+# Execute apt-vim init
+cd ${HOME}/apt-vim
+sudo python apt-vim init
 cd $start_dir
+
+# Restore stdin / stdout
+exec 1<&5
+exec 2<&6
+
+echo "${PATH}:${HOME}/.vimpkg/bin"
