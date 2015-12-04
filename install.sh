84e7b652d65281c85f57d7e83df7d5a7a70228f6
egalpin <egalpin@Evans-MacBook-Pro.local>
2015-12-04 12:25:41 -0500

    Added logging for test

--- install.sh
+++ install.sh
@@ -50,6 +50,8 @@ os.chdir(APT_VIM_DIR)
 aptvim = imp.load_source("aptvim", "./apt-vim")
 av = aptvim.aptvim(ASSUME_YES=True, VIM_CONFIG='', INSTALL_TARGET='')
 av.first_run()
+print('handle_install')
+print(av.handle_install)
 av.handle_install(None, None, None)
 EOF
 cd $start_dir
