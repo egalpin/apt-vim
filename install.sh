82ab4e5cf40186b1928b6e6d13328b50d977c3dc
Evan Galpin <egalpin@canonlabs.io>
2016-06-28 13:59:48 -0400

    [#39] Update install.sh
    - remove all instances of sudo

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #! /usr/bin/env sh
 
 start_dir=$(pwd)
+bin_string="PATH=${PATH}:${HOME}/.vimpkg/bin"
 
-if [ -f /etc/profile ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/profile) -eq 0 ]; then
-    sudo chmod 666 /etc/profile
-    sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/profile
-    sudo chmod 444 /etc/profile
-fi
+#if [ -f /etc/profile ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/profile) -eq 0 ]; then
+    #sudo chmod 666 /etc/profile
+    #sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/profile
+    #sudo chmod 444 /etc/profile
+#fi
 
-if [ -f /etc/bash.bashrc ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/bash.bashrc) -eq 0 ]; then
-    sudo chmod 666 /etc/bash.bashrc
-    sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/bash.bashrc
-    sudo chmod 444 /etc/bash.bashrc
-fi
+#if [ -f /etc/bash.bashrc ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/bash.bashrc) -eq 0 ]; then
+    #sudo chmod 666 /etc/bash.bashrc
+    #sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/bash.bashrc
+    #sudo chmod 444 /etc/bash.bashrc
+#fi
 
 # Download the apt-vim files
 curl -fSsLo ${HOME}/apt-vim/apt-vim --create-dirs \
@@ -32,12 +33,27 @@ if [ $(grep -c "call pathogen#helptags()" ${HOME}/.vimrc) -eq 0 ]; then
     echo "call pathogen#helptags()" >> ${HOME}/.vimrc
 fi
 
-# Update path for current shell
-export PATH=${PATH}:${HOME}/.vimpkg/bin
+# Update path for executing shell
+export "$bin_string"
+
+added_to_profile=false
+already_present=false
+for rc in bashrc zshrc bash_profile; do
+  if [ -s "$HOME/.$rc" ]; then
+    if grep -q "$bin_string" "$HOME/.$rc"; then
+      printf "== apt-vim PATH already installed in '~/.$rc'\n"
+      already_present=true
+    else
+      printf "\n$bin_string\n" >> "$HOME/.$rc"
+      printf "== Added apt-vim PATH to '~/.$rc'\n"
+      added_to_profile=true
+    fi
+  fi
+done
 
 # Execute apt-vim init
 cd ${HOME}/apt-vim
-sudo -E python - <<EOF
+python - <<EOF
 import imp, os
 print('apt-vim setup starting')
 HOME = os.path.expanduser("~")
@@ -52,4 +68,22 @@ av = aptvim.aptvim(ASSUME_YES=True, VIM_CONFIG='', INSTALL_TARGET='')
 av.first_run()
 av.handle_install(None, None, None)
 EOF
+
 cd $start_dir
+added_to_bin=false
+cp ${HOME}/apt-vim/apt-vim ${HOME}/.vimpkg/bin && added_to_bin=true
+
+
+if [ "$added_to_profile" = false ] && [ "$already_present" = false ]; then
+    echo "== Error:"
+    echo "   Found no profile to add apt-vim PATH to."
+    echo "   Add the following line to your shell profile and source it to install manually:"
+    printf "   $exec_string\n"
+elif [ "$added_to_bin" = false ]; then
+    echo "== Error:"
+    echo "   Failed to add apt-vim to ${HOME}/.vimpkg/bin"
+    echo "   Please add ${HOME}/apt-vim/apt-vim to PATH."
+else
+    echo "== apt-vim installation succeeded! Run 'source ~/.bashrc || source ~/.bash_profile' or 'source ~/.zshrc'"
+    echo "   to access the executable script."
+fi
