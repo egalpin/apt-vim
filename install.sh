478c806fb6aebdfa605b568a3327486f80b0fa9f
Evan Galpin <egalpin@canonlabs.io>
2016-06-28 14:32:34 -0400

    [#39] Move SCRIPT_EXE_PATH

--- install.sh
+++ install.sh
@@ -1,23 +1,11 @@
 #! /usr/bin/env sh
 
 start_dir=$(pwd)
-bin_string="PATH=${PATH}:${HOME}/.vimpkg/bin"
-
-#if [ -f /etc/profile ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/profile) -eq 0 ]; then
-    #sudo chmod 666 /etc/profile
-    #sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/profile
-    #sudo chmod 444 /etc/profile
-#fi
-
-#if [ -f /etc/bash.bashrc ] && [ $(sudo grep -c "export PATH=\$PATH:${HOME}/.vimpkg/bin" /etc/bash.bashrc) -eq 0 ]; then
-    #sudo chmod 666 /etc/bash.bashrc
-    #sudo echo "export PATH=\$PATH:${HOME}/.vimpkg/bin" >> /etc/bash.bashrc
-    #sudo chmod 444 /etc/bash.bashrc
-#fi
+bin_string="export PATH=${PATH}:${HOME}/.vimpkg/bin"
 
 # Download the apt-vim files
 curl -fSsLo ${HOME}/apt-vim/apt-vim --create-dirs \
-    https://raw.githubusercontent.com/egalpin/apt-vim/master/apt-vim
+    https://raw.githubusercontent.com/egalpin/apt-vim/39_bug_remove_sudo_dependence/apt-vim
 
 curl -fSsLo ${HOME}/apt-vim/vim_config.json \
     https://raw.githubusercontent.com/egalpin/apt-vim/master/vim_config.json
@@ -34,14 +22,14 @@ if [ $(grep -c "call pathogen#helptags()" ${HOME}/.vimrc) -eq 0 ]; then
 fi
 
 # Update path for executing shell
-export "$bin_string"
+eval "$bin_string"
 
 added_to_profile=false
 already_present=false
 for rc in bashrc zshrc bash_profile; do
   if [ -s "$HOME/.$rc" ]; then
     if grep -q "$bin_string" "$HOME/.$rc"; then
-      printf "== apt-vim PATH already installed in '~/.$rc'\n"
+      printf "== apt-vim PATH already exported in '~/.$rc'\n"
       already_present=true
     else
       printf "\n$bin_string\n" >> "$HOME/.$rc"
@@ -68,21 +56,18 @@ av = aptvim.aptvim(ASSUME_YES=True, VIM_CONFIG='', INSTALL_TARGET='')
 av.first_run()
 av.handle_install(None, None, None)
 EOF
+python_result=$?
 
 cd $start_dir
-added_to_bin=false
-cp ${HOME}/apt-vim/apt-vim ${HOME}/.vimpkg/bin && added_to_bin=true
 
-
-if [ "$added_to_profile" = false ] && [ "$already_present" = false ]; then
+if [ "$python_result" -ne 0 ]; then
+    echo "== Error:"
+    echo "   Installation failed."
+elif [ "$added_to_profile" = false ] && [ "$already_present" = false ]; then
     echo "== Error:"
     echo "   Found no profile to add apt-vim PATH to."
     echo "   Add the following line to your shell profile and source it to install manually:"
     printf "   $exec_string\n"
-elif [ "$added_to_bin" = false ]; then
-    echo "== Error:"
-    echo "   Failed to add apt-vim to ${HOME}/.vimpkg/bin"
-    echo "   Please add ${HOME}/apt-vim/apt-vim to PATH."
 else
     echo "== apt-vim installation succeeded! Run 'source ~/.bashrc || source ~/.bash_profile' or 'source ~/.zshrc'"
     echo "   to access the executable script."
